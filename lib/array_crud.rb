19163c2928d6721fbf74cbc9099ab8a882a1021f
Cesar Lara <cesar.lara@yale.edu>
2020-05-20 20:11:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,7 +35,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["I", "am", "learning", "new"]
+  array = ["wow", "I", "am", "learning", "new"]
   array[0]
 end
 
