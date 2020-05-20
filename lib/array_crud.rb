07a7ea83386db2eababf25a976ed596ac8ddb4cd
Cesar Lara <cesar.lara@yale.edu>
2020-05-20 19:00:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
-end
+  array = ["wow", "I", "am","really", "learning"]
+  element = "arrays!"
+  array.push(element)
+end 
 
 def add_element_to_start_of_array(array, element)
   array = ["wow", "I", "am","really", "learning"]
