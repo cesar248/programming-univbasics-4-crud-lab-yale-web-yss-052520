07c3273d0cb75eb6ad6bc4a162e3011f21a14125
Cesar Lara <cesar.lara@yale.edu>
2020-05-20 20:06:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["I", "am", "learning", "new"]
+  array = ["I", "am", "learning", "new", "arrays!"]
   array.pop
 end
 
